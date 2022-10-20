3dd19c5c214d1da38f679c45acdba3a0ad680c1f
Chris Denton <christophersdenton@gmail.com>
2022-10-20 17:50:55 +0100

    Documentation

--- crates/omnipath/src/lib.rs
+++ crates/omnipath/src/lib.rs
@@ -1,3 +1,33 @@
+//!
+//! # Absolute paths
+//!
+//! [`sys_absolute`] allows you to make a path absolute without needing the path to exist.
+//! This uses the rules of the current platform.
+//!
+//! ```
+//! // Makes a path absolute acording to the rules of the current OS.
+//! // It does not resolve symlinks and on Windows it won't return a verbatim
+//! // path unless given one.
+//! omnipath::sys_absolute("path/to/.//file".as_ref());
+//! ```
+//!
+//! # Windows verbatim paths
+//!
+//! Verbatim paths are paths that start with `\\?\`. For example `\\?\C:\path\to\file`.
+//! These paths can be used in Windows API calls but they are not what the user expects to see.
+//! You can turn these paths into more familiar user paths using the [`to_winuser_path`](crate::WinPathExt::to_winuser_path).
+//!
+//! ```
+//! #[cfg(windows)]
+//! {
+//!     use omnipath::windows::WinPathExt;
+//!     use std::path::Path;
+//!
+//!     let verbatim = Path::new(r"\\?\C:\path\to\file.txt");
+//!     let user_path = verbatim.to_winuser_path();
+//!     // user_path is Ok(r"C:\path\to\file.txt")
+//! }
+//! ```
 #![no_std]
 #![allow(clippy::single_char_pattern)]
 
@@ -16,3 +46,29 @@ pub use posix::PosixPathExt;
 #[cfg(any(doc, all(windows, feature = "std")))]
 #[doc(no_inline)]
 pub use windows::WinPathExt;
+
+/// Converts a path to absolute according to the rules of the current platform.
+///
+/// Unlike [`std::fs::canonicalize`] this does not resolve symlinks.
+///
+/// # Example
+///
+/// ```
+/// use omnipath::sys_absolute;
+/// use std::path::Path;
+/// use std::env::current_dir;
+/// let path = Path::new(r"path/to/.//file");
+/// assert_eq!(
+///     sys_absolute(path).unwrap(),
+///     // WARNING: This may not always be equal depending on the current
+///     // directory and OS.
+///     current_dir().unwrap().join("path/to/file")
+/// );
+/// ```
+#[cfg(feature = "std")]
+pub fn sys_absolute(path: &std::path::Path) -> std::io::Result<std::path::PathBuf> {
+	#[cfg(unix)]
+	return PosixPathExt::posix_absolute(path);
+	#[cfg(windows)]
+	return WinPathExt::win_absolute(path);
+}
